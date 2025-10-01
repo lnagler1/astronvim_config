6ac1aa84dae83e4201d4f64edb47781d25fad629
Lukas Nagler <lukasnagler17@gmail.com>
2025-10-01 11:30:33 +0200

    Added plugins mini-comment and theme gruvbox-baby

---
+++ lua/plugins/mini.comment.lua
@@ -0,0 +1,13 @@
+return {
+  "echasnovski/mini.comment",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
+  event = "User AstroFile",
+  opts = {
+    custom_commentstring = function()
+      return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+    end,
+  },
+  specs = {
+    { "numToStr/Comment.nvim", optional = true, enabled = false },
+  },
+}
